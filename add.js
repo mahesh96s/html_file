28b12b31ac865ee20885357c132e00fcdd3d8c9b
mahesh <42643630+mahesh96s@users.noreply.github.com>
2018-10-15 13:13:01 +0530

    updated code

--- add.js
+++ add.js
@@ -1,12 +1,9 @@
-var i=0;
-var x = new Array();
 $(document).ready(function() {  
   $(".add").click(function() {
     var name= $("#name").val();
     var age= $("#age").val();
     var add= "<tr><td>"+name+"</td><td>"+age+"</td></tr>";
-    $("table tbody").append(add);
-    var a=store(name,age);    
+    $("table tbody").append(add);      
     document.getElementById('name').value = ''; 
     document.getElementById('age').value = '';
   });    
